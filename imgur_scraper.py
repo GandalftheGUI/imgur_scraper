34abc7bf582e22b106cd76be1b52a6e2e89bca6d
Ian Remillard <Doom-Mac@Ians-iMac.local>
2017-09-28 20:41:37 -0700

    cleanup

--- imgur_scraper.py
+++ imgur_scraper.py
@@ -3,32 +3,11 @@ import time
 import urllib
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver import DesiredCapabilities
-from selenium.webdriver.common.action_chains import ActionChains
-
-
-
 
 address_input = raw_input("Address: ")
-
 chromedriver_location = './assets/chromedriver'
-# chrome_options = Options()
-# chrome_options.add_argument('--dns-prefetch-disable')
-# chrome_options.add_argument('--no-sandbox')
-# chrome_options.add_argument('--lang=en-US')
-
-# chrome_prefs = {
-#     'intl.accept_languages': 'en-US'
-# }
-#chrome_options.add_experimental_option('prefs', chrome_prefs)
-driver = webdriver.Chrome(chromedriver_location)#, chrome_options=chrome_options)
-#driver.implicitly_wait(page_delay)
-
+driver = webdriver.Chrome(chromedriver_location)
 driver.get(address_input)
-
-#if .js-post-truncated present?
-  #load all
 
 try:
   driver.find_element_by_class_name("js-post-truncated").click()
@@ -71,7 +50,6 @@ try:
       break
     else:
       last_element_id = new_last_element_id
-
 
 except NoSuchElementException:
   print('Images not found')
