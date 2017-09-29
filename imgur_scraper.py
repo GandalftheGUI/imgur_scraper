ebc2434fee4efd6d006b8c5dd82f5a1bb251fbe8
Ian Remillard <Doom-Mac@Ians-iMac.local>
2017-09-28 19:30:09 -0700

    scrolling to end of list

--- imgur_scraper.py
+++ imgur_scraper.py
@@ -4,6 +4,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.common.action_chains import ActionChains
+import time
 
 address_input = raw_input("Address: ")
 
@@ -17,29 +18,48 @@ chromedriver_location = './assets/chromedriver'
 #     'intl.accept_languages': 'en-US'
 # }
 #chrome_options.add_experimental_option('prefs', chrome_prefs)
-browser = webdriver.Chrome(chromedriver_location)#, chrome_options=chrome_options)
-#browser.implicitly_wait(page_delay)
+driver = webdriver.Chrome(chromedriver_location)#, chrome_options=chrome_options)
+#driver.implicitly_wait(page_delay)
 
-browser.get(address_input)
+driver.get(address_input)
 
 #if .js-post-truncated present?
   #load all
 
 try:
-  browser.find_element_by_class_name("js-post-truncated").click()
+  driver.find_element_by_class_name("js-post-truncated").click()
 except NoSuchElementException:
   print('All images loaded')
 
 try:
   post_image_elements = []
-  post_image_elements = browser.find_elements_by_class_name("post-image-container")
-  actions = ActionChains(browser)
+  last_element_id = ""
 
-  print('Found {}'.format(len(post_image_elements)))
+  
+  actions = ActionChains(driver)
 
-  for element in post_image_elements:
-    print('Moving to post-image with id: {}'.format(element.get_attribute("id")))
-    actions.move_to_element(element).perform()
+  
+  while True:
+    post_image_elements = driver.find_elements_by_class_name("post-image-container")
+    last_element = post_image_elements[-1]
+    new_last_element_id = last_element.get_attribute("id")
+
+    print('Moving to post-image with id: {}'.format(last_element_id))
+    driver.execute_script("arguments[0].scrollIntoView();", last_element)
+    time.sleep(2)
+    
+    if new_last_element_id == last_element_id:
+      print('End of list!')
+      break
+    else:
+      last_element_id = new_last_element_id
+
+
+  #print('Found {}'.format(len(post_image_elements)))
+  #for element in post_image_elements:
+  #actions.move_to_element(last_element).perform()
+  
+
 
 except NoSuchElementException:
   print('Images not found')
