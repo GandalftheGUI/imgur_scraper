2380dede436b94f8fbb11720ac347b5b5a176826
Ian Remillard <Doom-Mac@Ians-iMac.local>
2017-09-28 19:54:37 -0700

    finding all image urls

--- imgur_scraper.py
+++ imgur_scraper.py
@@ -33,31 +33,44 @@ except NoSuchElementException:
 
 try:
   post_image_elements = []
+  image_address_hash = {}
   last_element_id = ""
-
-  
-  actions = ActionChains(driver)
-
   
   while True:
     post_image_elements = driver.find_elements_by_class_name("post-image-container")
+    #get ids
+
+    #there will probably be repeats but meh who cares hash uniqueness will take take of it
+    for element in post_image_elements:
+      image_id = element.get_attribute("id")
+      image_source = element.find_element_by_css_selector("img").get_attribute("src")
+      if image_address_hash.has_key(image_id):
+        pass
+        #print('image id {} already saved'.format(image_id))
+      else:
+        image_address_hash[image_id] = image_source
+        print('[{}] -> {}'.format(image_id, image_source))
+
+
     last_element = post_image_elements[-1]
     new_last_element_id = last_element.get_attribute("id")
 
+
+
     print('Moving to post-image with id: {}'.format(last_element_id))
     driver.execute_script("arguments[0].scrollIntoView();", last_element)
-    time.sleep(2)
-    
+    time.sleep(1)
+
     if new_last_element_id == last_element_id:
       print('End of list!')
+      print('{} images found'.format(len(image_address_hash)))
       break
     else:
       last_element_id = new_last_element_id
 
-
+  #actions = ActionChains(driver)
   #print('Found {}'.format(len(post_image_elements)))
-  #for element in post_image_elements:
-  #actions.move_to_element(last_element).perform()
+    #actions.move_to_element(last_element).perform()
   
 
 
